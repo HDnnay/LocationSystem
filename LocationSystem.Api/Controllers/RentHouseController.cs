67bf28839289a9d847b325f5272e71ce515a59a7
meiyi <meiyi@DESKTOP-759F2PU>
2026-02-01 16:04:10 +0800

    RentHouse控制器获取列表相关

---
+++ LocationSystem.Api/Controllers/RentHouseController.cs
@@ -0,0 +1,39 @@
+﻿using LocationSystem.Application.Features.RentHousies.Queries.GetRentHouseList;
+using LocationSystem.Application.Features.RentHousies.Queries.QueryRentHouseList;
+using LocationSystem.Application.Utilities;
+using LocationSystem.Application.Utilities.Common;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LocationSystem.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RentHouseController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public RentHouseController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Get(GetRentHouseListFilter request)
+        {
+            var query = new GetRentHouseListQuery()
+            {
+                Page = request.Page,
+                PageSize = request.PageSize,
+                keyWord = request.keyWord
+            };
+            var data = await _mediator.Send(query);
+            return Ok(data);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Post()
+        {
+            return Ok();
+        }
+
+        
+    }
+}
