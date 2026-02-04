cf9c129efaa49ab777413b458c04bb23b7043460
meiyi <meiyi@DESKTOP-759F2PU>
2026-02-05 00:40:15 +0800

    add

--- LocationSystem.Api/Program.cs
+++ LocationSystem.Api/Program.cs
@@ -68,7 +68,7 @@ builder.Services.AddHostedService<RabbitMQTestService>();
 //处理数据库sqlite迁移值系统持久化
 //builder.Services.AddHostedService<DatabaseInitializerServices>();
 //更新数据库省份字段，数据大约有10000条
-builder.Services.AddHostedService<CompanyUpdateBackgroundService>();
+//builder.Services.AddHostedService<CompanyUpdateBackgroundService>();
 
 var app = builder.Build();
 app.UseIpRateLimiting();
