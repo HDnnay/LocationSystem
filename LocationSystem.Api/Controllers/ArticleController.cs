81d359be38a5ca49a1de27485b1c1f62386a09b5
meiyi <meiyi@DESKTOP-759F2PU>
2026-04-22 21:02:13 +0800

    文章控制器

---
+++ LocationSystem.Api/Controllers/ArticleController.cs
@@ -0,0 +1,17 @@
+﻿using LocationSystem.Application.Utilities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LocationSystem.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArticleController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public ArticleController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+    }
+}
