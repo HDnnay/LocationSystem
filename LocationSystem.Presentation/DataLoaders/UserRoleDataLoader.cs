6cecacd8645c036b9bde1c152798bfa9e5176f9d
meiyi <meiyi@DESKTOP-759F2PU>
2026-04-25 09:29:41 +0800

    修改映射

--- LocationSystem.Presentation/DataLoaders/UserRoleDataLoader.cs
+++ LocationSystem.Presentation/DataLoaders/UserRoleDataLoader.cs
@@ -1,19 +1,30 @@
-﻿using LocationSystem.Application.Features.Roles.Queries.GetRolesByUser;
+﻿using GreenDonut;
+using LocationSystem.Application.Features.Roles.Queries.GetRolesByUser;
 using LocationSystem.Application.GrapqLDTOs.Roles;
 using LocationSystem.Application.Utilities;
 
 namespace LocationSystem.Presentation.DataLoaders
 {
-    public static class UserRoleDataLoader
+    public class UserRoleDataLoader : BatchDataLoader<Guid, List<RoleGraphqLDto>>
     {
-        [DataLoader]
-        public static Task<Dictionary<Guid, List<RoleGraphqLDto>>> GetRolesByUserAsync(
-            IReadOnlyList<Guid> ids,
-            [Service] IMediator mediator,
-            CancellationToken cancellationToken = default)
+        private readonly IMediator _mediator;
+
+        public UserRoleDataLoader(
+            IMediator mediator,
+            IBatchScheduler batchScheduler,
+            DataLoaderOptions? options = null)
+            : base(batchScheduler, options)
         {
-            var query = new GetRolesByUserQuery() { Ids = ids };
-            return mediator.Send(query);
+            _mediator = mediator;
+        }
+
+        protected override async Task<IReadOnlyDictionary<Guid, List<RoleGraphqLDto>>> LoadBatchAsync(
+            IReadOnlyList<Guid> keys,
+            CancellationToken cancellationToken)
+        {
+            var query = new GetRolesByUserQuery { Ids = keys };
+            var result = await _mediator.Send(query);
+            return result;
         }
     }
 }
