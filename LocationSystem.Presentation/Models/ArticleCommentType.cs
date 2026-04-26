bcabb736e1e9251125b984fe46574b49b28df8ee
meiyi <meiyi@DESKTOP-759F2PU>
2026-04-26 16:56:36 +0800

    add

--- LocationSystem.Presentation/Models/ArticleCommentType.cs
+++ LocationSystem.Presentation/Models/ArticleCommentType.cs
@@ -7,8 +7,8 @@ namespace LocationSystem.Presentation.Models
         protected override void Configure(IObjectTypeDescriptor<ArticleCommentGraphqLDto> descriptor)
         {
             descriptor.Name("articleComment");
-            descriptor.Field(t => t.Id).Type<NonNullType<IdType>>().Description("评论ID");
-            descriptor.Field(t => t.UserId).Type<NonNullType<IdType>>().Description("用户ID");
+            descriptor.Field(t => t.Id).Type<NonNullType<IdType>>().Description("评论Id");
+            descriptor.Field(t => t.UserId).Type<NonNullType<IdType>>().Description("用户Id");
             descriptor.Field(t => t.Comment).Type<NonNullType<StringType>>().Description("评论内容");
             descriptor.Field(t => t.IsVisiable).Type<NonNullType<BooleanType>>().Description("是否可见");
             descriptor.Field(t => t.CreateTime).Type<NonNullType<DateTimeType>>().Description("创建时间");
