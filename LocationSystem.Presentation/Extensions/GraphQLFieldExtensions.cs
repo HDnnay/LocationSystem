8e8818151e07cc0e65c7237b5f6c782689af96fe
meiyi <meiyi@DESKTOP-759F2PU>
2026-04-27 00:42:56 +0800

    中间件

---
+++ LocationSystem.Presentation/Extensions/GraphQLFieldExtensions.cs
@@ -0,0 +1,35 @@
+﻿// LocationSystem.Presentation/Extensions/GraphQLFieldExtensions.cs
+namespace LocationSystem.Presentation.Extensions
+{
+    public static class GraphQLFieldExtensions
+    {
+        /// <summary>
+        /// 为字段设置权限要求
+        /// </summary>
+        public static IObjectFieldDescriptor WithPermission(
+            this IObjectFieldDescriptor descriptor,
+            string permissionCode)
+        {
+            descriptor.Extend().OnBeforeCreate(d =>
+           {
+               d.ContextData["RequiredPermission"] = permissionCode;
+           });
+            return descriptor;
+        }
+
+        /// <summary>
+        /// 为字段设置多个权限要求
+        /// </summary>
+        public static IObjectFieldDescriptor WithPermissions(
+            this IObjectFieldDescriptor descriptor,
+            params string[] permissionCodes)
+        {
+            descriptor.Extend().OnBeforeCreate(d =>
+           {
+               d.ContextData["RequiredPermissions"] = permissionCodes;
+               d.ContextData["RequireAllPermissions"] = false; // 或 true
+           });
+            return descriptor;
+        }
+    }
+}
--- LocationSystem.Presentation/GraphQL/QueryType.cs
+++ LocationSystem.Presentation/GraphQL/QueryType.cs
@@ -1,19 +1,20 @@
-﻿namespace LocationSystem.Presentation.GraphQL
+﻿using LocationSystem.Presentation.Extensions;
+using LocationSystem.Presentation.Models;
+
+namespace LocationSystem.Presentation.GraphQL
 {
 
     public class QueryType : ObjectType<Query>
     {
 
-        //protected override void Configure(IObjectTypeDescriptor descriptor)
-        //{
-        //    descriptor.Field("Users").UsePaging<UserType>()
-        //        .Resolve(async context =>
-        //        {
-        //            var query = new GetUsersQuery();
-        //            var result = await context.Service<IMediator>().Send(query);
-        //            return result.Select(t => t.Adapt<UserType>());
-        //        });
-        //}
+        protected override void Configure(IObjectTypeDescriptor<Query> descriptor)
+        {
+            descriptor.Name("Query");
+            descriptor.Field(q => q.GetArticles(default!))
+                .Type<ListType<ArticleType>>()
+                .WithPermission("article.list.read")
+                .Description("获取文章列表，支持过滤和排序");
+        }
     }
 
 }
